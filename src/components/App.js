130300a0dc5fbde863caf90822fdef1d4cdf0b78
Vikas Verma <vikasverma15@gmail.com>
2026-01-05 18:39:54 +0530

    adding component

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,20 @@
-
 import React from "react";
-import './../styles/App.css';
+import { Routes, Route } from "react-router-dom";
+import ItemList from "./ItemList";
+import ItemDetail from "./ItemDetail";
+import "./../styles/App.css";
 
 const App = () => {
   return (
-    <div>
-        {/* Do not remove the main div */}
+    <div id="main">
+      {/* Do not remove the main div */}
+      <Routes>
+        <Route path="/" element={<ItemList />} />
+        <Route path="/items/:id" element={<ItemDetail />} />
+      </Routes>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
+
